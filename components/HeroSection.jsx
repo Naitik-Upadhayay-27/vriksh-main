e2b1b5bc07a14fcd0d65fe1a6a3cb3016d74fe57
Naitik-Upadhayay-27 <upadhayaynaitik2712@gmail.com>
2025-04-07 12:18:43 -0700

    commit

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -38,14 +38,21 @@ const PropertyCard = ({ property }) => {
 };
 
 export default function HeroSection() {
-  // Initialize Lenis
-  const lenis = new Lenis({
-    autoRaf: true,
-  });
-  // Listen for the scroll event and log the event data
-  lenis.on("scroll", (e) => {
-    console.log(e);
-  });
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const lenis = new Lenis({
+        autoRaf: true,
+      });
+
+      lenis.on("scroll", (e) => {
+        console.log(e);
+      });
+
+      return () => {
+        lenis.destroy();
+      };
+    }
+  }, []);
 
   const trendingArticles = [
     {
